db08dafa9892e57ca6a8155390f9673f34a366c5
Ariel Fonticiella <ariel@fonticiella.io>
2018-03-27 04:26:33 -0400

    tooling with Prettier

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -10,7 +10,11 @@ const MyFirstComponent = function() {
     { id: 'my-first-component' },
     ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
     ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+    ce(
+      MyTitle,
+
+      { title: 'Rick and Morty', color: 'LimeGreen' }
+    ),
     ce(MyTitle, { title: 'Silicon Valley', color: 'Peru' })
     // createElement has "variable errity" which means you can give it as many perameters as you want.
   );

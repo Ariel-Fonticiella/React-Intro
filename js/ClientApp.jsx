97c8a9b909c29c50f202faab3ff5fde4122e2110
Ariel Fonticiella <ariel@fonticiella.io>
2018-03-28 16:50:51 -0400

    fixed some bugs with prettier and ESlint

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,25 @@
+import React from "react";
+import { render } from "react-dom";
+
 const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
 };
 
 const MyFirstComponent = function() {
   return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'Peru' })
-    // createElement has "variable errity" which means you can give it as many perameters as you want.
+    "div",
+    { id: "my-first-component" },
+    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
+    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "LimeGreen" }),
+    ce(MyTitle, { title: "House of Cards", color: "peru" })
   );
 };
 
-let x = 5;
-
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById("app"));

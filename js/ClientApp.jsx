101048ca4a3664ca64f8b9f0c0d381f34fa6be39
Ariel Fonticiella <ariel@fonticiella.io>
2018-03-29 03:01:32 -0400

    Created landing page jsx, and created route to Landing.jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { HashRouter, Route } from "react-router-dom";
+import Landing from "./Landing";
 
 const App = () => (
-  <div className="app">
-    <div className="landing">
-      <h1>svideo</h1>
-      <input type="text" placeholder="Search" />
-      <a>or Browse All</a>
+  <HashRouter>
+    <div className="app">
+      {/* This is one route of our application. It's saying that the path to get there is "/" so it's the home page. And the "exact" means it must "exactly" match that path. If "exact" was not included, if you made a request to "/foo" it would still match the "/" path. So if the left side of it matches the route, it's going to match on it. This is why you need to do "exact". */}
+
+      <Route exact path="/" component={Landing} />
     </div>
-  </div>
+  </HashRouter>
 );
 
 render(<App />, document.getElementById("app"));
